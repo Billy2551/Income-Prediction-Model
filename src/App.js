e9fcf24affb41337918299cbb85520e2b00bb20b
Bill Wang <bwang0536@berkeley.edu>
2020-12-02 21:37:36 +0800

    Sort of working react page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import FetchIncome from './FetchIncome';
 
 function App() {
+// "company", "jobTitle", "school", "state", "major", "gender"
+  var range = null;
+  const options = [
+    { option1: "1" },
+    { option2: "2" },
+    { option3: "3" },
+    { option4: "4" }
+  ];
+
+  const fetchIncome = () => {
+        const url = "https://edith-delta-api.herokuapp.com/test/this/will/work";
+        const response = fetch(url, {headers: {"accept": "application/json",'Access-Control-Allow-Origin':'*'}});
+        const data = response.json();
+        console.log(data);
+        range = data;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+      <br></br>
+      <br></br>
+      <br></br>
+        School: 
+        <select id='school'>
+        <option selected value="None"></option>
+          <option value="berkeley">University of California, Berkeley</option>
+          <option value="stanford">Stanford</option>
+          <option value="mit">MIT</option>
+        </select>
+        <br></br>
+        <br></br>
+        Major:
+        <select id='major'>
+        <option selected value="None"></option>
+          <option value="cs">Computer Science</option>
+          <option value="ee">Electrical Engineering</option>
+          <option value="ba">Business Administration</option>
+        </select>
+        <br></br>
+        <br></br>
+        Highest Level of Education:
+        <select id='education'>
+        <option selected value="None"></option>
+          <option value="bach">Bachelors</option>
+          <option value="mast">Masters</option>
+          <option value="doc">Doctors</option>
+        </select>
+        <br></br>
+        <br></br>
+        Graduation Year:
+        <select id="year"></select>
+        <option value={Object.values(options)}>
+              {" "}
+              {Object.keys(options)}{" "}
+            </option>
+        <br></br>
+        <br></br>
+        Gender:
+        <select>
+        <option selected value="None"></option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+        <br></br>
+        <br></br>
+        Location:
+        <input type="text" name="loc"/>
+        <br></br>
+        <br></br>
+        Industry: 
+        <select>
+          <option selected value="None"></option>
+          <option value="ba">Business Analyst</option>
+          <option value="con">Consultant</option>
+          <option value="data">Data Analyst</option>
+          <option value="ib">Investment Banking</option>
+          <option value="prod">Product Developer</option>
+          <option value="prom">Project Manager</option>
+          <option value="quant">Quantitative Analyst</option>
+          <option value="swe">Software Engineer</option>
+        </select>
+      </form>
+    <div>
+                <FetchIncome />
+            </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/FetchIncome.js
@@ -0,0 +1,65 @@
+import React from "react";
+import App from "./App"
+
+export default class FetchIncome extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: true,
+            range: null,
+            badInput: false
+        };
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    async handleClick() {
+        try {
+        this.setState({range: {major: "Calculating...", location:"", industry:""}})
+        const url = "https://edith-delta-api.herokuapp.com/test/" + document.getElementById("school").value + "/" + document.getElementById("major").value + "/" + document.getElementById("year").value;
+        const response = await fetch(url, {headers: {"accept": "application/json",'Access-Control-Allow-Origin':'*'}});
+        const data = await response.json();
+        console.log(data);
+        this.setState({range: data, loading: false, badInput: false});
+        console.log(document.getElementById("school").value)
+        }
+        catch(error) {
+            this.setState({badInput: true});
+        }
+        // var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+        // targetUrl = 'https://edith-delta-api.herokuapp.com/test/this/will/work'
+        // fetch(proxyUrl + targetUrl, {headers: {"accept": "application/json","Access-Control-Allow-Origin": "*"}})
+        //     .then(blob => blob.json())
+        //     .then(data => {
+        //         console.table(data);
+        //         document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+        //         this.setState({range: data, loading: false});
+        //         return data;
+        //     })
+        //     .catch(e => {
+        //         console.log(e);
+        //         return e;
+        //     });
+    };
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                <button onClick={this.handleClick}>Submit</button>
+                <br></br>
+                <br></br>
+                <br></br>
+                {this.state.badInput ? <div>Please make sure to fill in all fields before submitting</div>:
+                    this.state.loading ? <div>Please enter your information</div> : 
+                    <div>
+                        {this.state.range.major}
+                        {this.state.range.location}
+                        {this.state.range.industry}
+                    </div>
+                    }
+            </div>
+        )
+    };
+
+}
